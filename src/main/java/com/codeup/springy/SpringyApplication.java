659465943f3776fe2f8cbfffbe8000f842bfac09
Louie Espinosa <louie.espinosa@proton.me>
2022-09-07 09:42:23 -0500

    created new intellij project using spring build

---
+++ src/main/java/com/codeup/springy/SpringyApplication.java
@@ -0,0 +1,13 @@
+package com.codeup.springy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/codeup/springy/SpringyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeup.springy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
